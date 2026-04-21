15c3591066541e23af3c16b188fb0061e3337140
noumanhafeez <noumanhafeez095@gmail.com>
2026-04-21 21:56:24 +0500

    chore: push transfer learning datacamp

---
+++ useless/transfer_learning_oneshot.py
@@ -0,0 +1,15 @@
+# Include an example in the input ext
+input_text = """
+Text: "The dinner we had was great and the service too."
+Classify the sentiment of this sentence as either positive or negative.
+Example:
+Text: "The food was delicious"
+Sentiment: Positive
+Text: "The dinner we had was great and the service too."
+Sentiment:
+"""
+
+# Apply the example to the model
+result = model(input_text, max_length=100)
+
+print(result[0]["label"])
